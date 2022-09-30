889c63fcd915276089ee06b2ea5d3b23dc6bea0b
Peterhabit <jiseokchoij@gmail.com>
2022-09-30 18:09:44 +0900

    feat: swift에서 break는 생략

---
+++ Control Flow/switch-case/01.swift
@@ -0,0 +1,16 @@
+var value = 2
+switch value
+{
+    case 0:
+        print("영")
+    case 1:
+        print("일")
+    case 2:
+        print("이")
+    case 3:
+        print("삼")    
+    default:
+        print("4이상")
+}
+
+// 이
