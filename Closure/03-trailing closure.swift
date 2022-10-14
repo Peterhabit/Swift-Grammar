c2636c529a0f74a594b6aa90ed49435b379c2ce3
Peterhabit <jiseokchoij@gmail.com>
2022-10-14 21:21:02 +0900

    Fix: 오타수정

--- Closure/03-trailing closure.swift
+++ Closure/03-trailing closure.swift
@@ -2,7 +2,7 @@ func someFun(cl: () -> Void) {
 }
 //argument type이 ()->()이다
 //()->() 함수를 클로저로 만든다면
-//c1이 함수의 마지막 argument이기에 argument labal 생략가능
+//c1이 함수의 마지막 argument이기에 argument label 생략가능
 //클로저의 식이 길때 유용
 
 // trailing closure를 사용 안하면
