2b1cf97900fecea11238376095f37ff2fb7f3f55
Peterhabit <jiseokchoij@gmail.com>
2022-10-14 21:16:45 +0900

    feat: void함수 후행 클로저 사용하여 호출

---
+++ Closure/03-trailing closure.swift
@@ -0,0 +1,16 @@
+func someFun(cl: () -> Void) {
+}
+//argument type이 ()->()이다
+//()->() 함수를 클로저로 만든다면
+//c1이 함수의 마지막 argument이기에 argument labal 생략가능
+//클로저의 식이 길때 유용
+
+// trailing closure를 사용 안하면
+someFun(cl: {
+//closure’s body
+})
+
+// trailing closure 사용
+someFun() {
+//trailing closure's body goes here
+}
