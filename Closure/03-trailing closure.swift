52423c5b5613ea336e24064e5629a5c6849600bd
Peterhabit <jiseokchoij@gmail.com>
2022-10-14 21:28:16 +0900

    Fix: 설명 수정

--- Closure/03-trailing closure.swift
+++ Closure/03-trailing closure.swift
@@ -1,9 +1,5 @@
 func someFun(cl: () -> Void) {
 }
-//argument type이 ()->()이다
-//()->() 함수를 클로저로 만든다면
-//c1이 함수의 마지막 argument이기에 argument label 생략가능
-//클로저의 식이 길때 유용
 
 // trailing closure를 사용 안하면
 someFun(cl: {
@@ -11,6 +7,10 @@ someFun(cl: {
 })
 
 // trailing closure 사용
+//클로저가 함수의 마지막 argument라면 후행 클로저 사용가능
+//c1이 함수의 마지막 argument이기에 argument label 생략가능
+//클로저의 식이 길때 유용
+
 someFun() {
 //trailing closure's body goes here
 }
